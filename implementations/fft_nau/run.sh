0e45b1e8cf155b86c881eff794ad4551b17efa63
KillByte <killbyte.prototype@gmail.com>
2022-04-24 23:40:49 +0100

    General commit

--- implementations/fft_nau/run.sh
+++ implementations/fft_nau/run.sh
@@ -6,4 +6,11 @@ if [ -z $1 ]; then
 fi
 NAU_BIN=/home/kb/Documents/MIEI/4_Grade/1_Sem/VI1/Projects/nau_bin/composerImGui.exe
 XML_PATH=$(realpath $TARGET)
-wine $NAU_BIN $XML_PATH
+
+# Check if prime-run command is available
+NVIDIA_PRIME_AVAILABLE=$(command -v prime-run)
+if [ -z "$NVIDIA_PRIME_AVAILABLE" ]; then
+    wine $NAU_BIN $XML_PATH
+else 
+    prime-run wine $NAU_BIN $XML_PATH
+fi
