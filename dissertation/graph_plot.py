0d7be6795fd826dc05857cf7de8a9f39e5ea5e7f
kb <killbyte.prototype@gmail.com>
2022-11-23 23:50:40 +0000

    Updated dissertation

--- dissertation/graph_plot.py
+++ dissertation/graph_plot.py
@@ -35,44 +35,56 @@ def ms_graph(title, sizes, data, filename=''):
 #     ]
 # )
 
-ms_graph("cuFFT vs GLSL Radix-2",
-    sizes=[128, 256, 512, 1024],
-    data=[
-        ("cuFFT", [0.0359, 0.0494, 0.1335, 0.5609]),
-        ("GLSL Radix-2 Cooley-Tukey", [0.073, 0.257, 1.032, 2.646]),
-        ("GLSL Radix-2 Stockham",[0.049, 0.135, 0.545, 2.341]),
-    ]
-    , filename='cufft_vs_glsl_radix2'
-)
+#################################
+
+# ms_graph("cuFFT vs GLSL Radix-2",
+#     sizes=[128, 256, 512, 1024],
+#     data=[
+#         ("cuFFT", [0.0359, 0.0494, 0.1335, 0.5609]),
+#         ("GLSL Radix-2 Cooley-Tukey", [0.073, 0.257, 1.032, 2.646]),
+#         ("GLSL Radix-2 Stockham",[0.049, 0.135, 0.545, 2.341]),
+#     ]
+#     , filename='cufft_vs_glsl_radix2'
+# )
 
 ms_graph("cuFFT vs GLSL Radix-4",
     sizes=[256, 1024],
     data=[
         ("cuFFT", [0.0494, 0.5609]),
+        ("GLSL Radix-2 Stockham",[0.135, 2.341]),
         ("GLSL Radix-4 Stockham",[0.087, 1.363]),
     ]
     , filename='cufft_vs_glsl_radix4'
 )
 
 
-ms_graph("CUDA Radix-2 vs GLSL Radix-2",
-    sizes=[128, 256, 512, 1024],
-    data=[
-        # CUDA
-        ("CUDA Radix-2 Cooley-Tukey", [0.057, 0.220,  0.944, 3.729]),
-        ("CUDA Radix-2 Stockham",[0.054, 0.209, 1.047, 4.813]),
-        # GLSL
-        ("GLSL Radix-2 Cooley-Tukey", [0.073, 0.257, 1.032, 2.646]),
-        ("GLSL Radix-2 Stockham",[0.049, 0.135, 0.545, 2.341]),
-    ]
-    , filename='cuda_vs_glsl_radix2'
-)
+# ms_graph("CUDA Radix-2 vs GLSL Radix-2",
+#     sizes=[128, 256, 512, 1024],
+#     data=[
+#         # CUDA
+#         ("CUDA Radix-2 Cooley-Tukey", [0.057, 0.220,  0.944, 3.729]),
+#         ("CUDA Radix-2 Stockham",[0.054, 0.209, 1.047, 4.813]),
+#         # GLSL
+#         ("GLSL Radix-2 Cooley-Tukey", [0.073, 0.257, 1.032, 2.646]),
+#         ("GLSL Radix-2 Stockham",[0.049, 0.135, 0.545, 2.341]),
+#     ]
+#     , filename='cuda_vs_glsl_radix2'
+# )
 
-ms_graph("cuFFT vs GLSL Radix-4",
-    sizes=[256, 1024],
-    data=[
-        ("CUDA Radix-4 Stockham", [0.123, 1.993]),
-        ("GLSL Radix-4 Stockham",[0.087, 1.363]),
-    ]
-    , filename='cuda_vs_glsl_radix4'
-)
+# ms_graph("CUDA vs GLSL Radix-4",
+#     sizes=[256, 1024],
+#     data=[
+#         ("CUDA Radix-4 Stockham", [0.123, 1.993]),
+#         ("GLSL Radix-4 Stockham",[0.087, 1.363]),
+#     ]
+#     , filename='cuda_vs_glsl_radix4'
+# )
+
+# ms_graph("Frame time Stage per pass vs unique pass FFT",
+#     sizes=[128, 256, 512, 1024],
+#     data=[
+#         ("Cooley-Tukey stage per pass", [1.361, 1.501, 1.604, 1.806]),
+#         ("Cooley-Tukey unique pass", [0.743, 0.752, 0.786, 0.79]),
+#     ]
+#     , filename='glsl_stage_pass_vs_unique_pass'
+# )
